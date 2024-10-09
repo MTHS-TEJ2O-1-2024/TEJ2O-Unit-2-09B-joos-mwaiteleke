4e3cea1577e9c6359ba736f2803330ec94ecafcc
joos-mwaiteleke <joos.mwaiteleke@mths.ca>
2024-10-09 11:23:42 -0400

    Made rock paper scissors

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Joos
+ * Created on: Oct 2024
+ * This program plays rock, paper, scissors
 */
 
-basic.showString('Hello, World!')
+
+let randomNumber: number = 0
+let score: number = 0
+
+randomNumber = -1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
+
+   
+    if (randomNumber == 0) {
+        basic.showIcon(IconNames.Square)
+    }
+
+    if (randomNumber == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+   
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+    }
+    basic.pause(1000)
+    basic.showIcon(IconNames.Happy)})
+
+input.onButtonPressed(Button.A, function() {
+    score = score+1
+    basic.showIcon(IconNames.Yes)
+})
+
+input.onButtonPressed(Button.B, function() {
+    basic.showString("Score:" + score .toString)
+})
